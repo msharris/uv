6b0bc31baa6b83bafef765991b10d8b7dab28c9d
Mitch Harris <mitchell.harris1995@gmail.com>
2024-03-10 19:58:15 +1100

    Parse API data into struct with correct types

--- main.go
+++ main.go
@@ -2,29 +2,97 @@ package main
 
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type Stations struct {
-	XMLName   xml.Name   `xml:"stations"`
-	Locations []Location `xml:"location"`
+type XMLStations struct {
+	XMLName   xml.Name      `xml:"stations"`
+	Locations []XMLLocation `xml:"location"`
 }
 
-type Location struct {
-	XMLName xml.Name `xml:"location"`
-	Id      string   `xml:"id,attr"`
-	Name    string   `xml:"name"`
-	Index   string   `xml:"index"`
-	Time    string   `xml:"time"`
-	Date    string   `xml:"date"`
-	Status  string   `xml:"status"`
+type XMLLocation struct {
+	XMLName     xml.Name `xml:"location"`
+	Id          string   `xml:"id,attr"`
+	Name        string   `xml:"name"`
+	Index       string   `xml:"index"`
+	Time        string   `xml:"time"`
+	Date        string   `xml:"date"`
+	FullDate    string   `xml:"fulldate"`
+	UTCDateTime string   `xml:"utcdatetime"`
+	Status      string   `xml:"status"`
 }
 
-func (loc Location) String() string {
-	return fmt.Sprintf("%-17v %-5v %v", loc.Id, loc.Index, loc.Time)
+type Station struct {
+	Id        string
+	Name      string
+	UVIndex   float64
+	Time      time.Time
+	Available bool
+}
+
+func (xmlStations *XMLStations) Stations() []Station {
+	var stations []Station
+	for _, xmlLoc := range xmlStations.Locations {
+		stations = append(stations, xmlLoc.Station())
+	}
+	return stations
+}
+
+func (xmlLoc *XMLLocation) Station() Station {
+	station := Station{
+		Id:        strings.ToUpper(xmlLoc.Name),
+		Name:      xmlLoc.Id,
+		Available: xmlLoc.Status == "ok",
+	}
+
+	station.UVIndex, _ = strconv.ParseFloat(xmlLoc.Index, 64)
+
+	station.Time, _ = time.Parse("2006/01/02 15:04", xmlLoc.UTCDateTime)
+	if locName, err := getLocationName(station.Name); err == nil {
+		loc, _ := time.LoadLocation(locName)
+		station.Time = station.Time.In(loc)
+	}
+
+	return station
+}
+
+func getLocationName(stationName string) (string, error) {
+	m := map[string]string{
+		"Adelaide":         "Australia/Adelaide",
+		"Alice Springs":    "Australia/Darwin",
+		"Brisbane":         "Australia/Brisbane",
+		"Canberra":         "Australia/Sydney",
+		"Casey":            "Antarctica/Casey",
+		"Darwin":           "Australia/Darwin",
+		"Davis":            "Antarctica/Davis",
+		"Emerald":          "Australia/Brisbane",
+		"Gold Coast":       "Australia/Brisbane",
+		"Kingston":         "Australia/Hobart",
+		"Macquarie Island": "Antarctica/Macquarie",
+		"Mawson":           "Antarctica/Mawson",
+		"Melbourne":        "Australia/Melbourne",
+		"Newcastle":        "Australia/Sydney",
+		"Perth":            "Australia/Perth",
+		"Sydney":           "Australia/Sydney",
+		"Townsville":       "Australia/Brisbane",
+	}
+
+	if name, ok := m[stationName]; !ok {
+		return name, errors.New(fmt.Sprintf("No time zone configured for station %v", stationName))
+	} else {
+		return name, nil
+	}
+}
+
+func (s Station) String() string {
+	return fmt.Sprintf("%-17v %4.1f  %v", s.Name, s.UVIndex, s.Time.Format("Mon 2 Jan 3:04 pm"))
 }
 
 func main() {
@@ -39,27 +107,14 @@ func main() {
 		log.Fatal("Unable to read ARPANSA data file")
 	}
 
-	var stations Stations
-	err = xml.Unmarshal(body, &stations)
+	var xmlStations XMLStations
+	err = xml.Unmarshal(body, &xmlStations)
 	if err != nil {
 		log.Fatal("Unexpected format of ARPANSA data file")
 	}
 
-	for _, loc := range stations.Locations {
-		fmt.Println(loc)
+	stations := xmlStations.Stations()
+	for _, s := range stations {
+		fmt.Println(s)
 	}
 }
-
-//<stations>
-//  ...
-//  <location id="Newcastle">
-//    <name>new</name>
-//    <index>0.0</index>
-//    <time>6:07 PM</time>
-//    <date>6/03/2024</date>
-//    <fulldate>Wednesday, 6 March 2024</fulldate>
-//    <utcdatetime>2024/03/06 08:07</utcdatetime>
-//    <status>ok</status>
-//  </location>
-//  ...
-//</stations>
